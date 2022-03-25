dfc3019fa9e7ae7b1bb5a02414fcba5d711ffe44
purpleaoaoa <98028277+purpleaoaoa@users.noreply.github.com>
2022-03-25 10:05:14 +0300

    Add files via upload

---
+++ 6 урок 1 задание.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace urok6
+{
+    class Program
+    {
+        enum DayOfWeek { MON, TUE, WED, THU, FRI, SAT, SUN }
+        static void PrintDayOfWeek(DayOfWeek dow)
+        {
+            switch (dow)
+            {
+                case DayOfWeek.MON:
+                    Console.WriteLine("Понедельник");
+                    break;
+                case DayOfWeek.TUE:
+                    Console.WriteLine("Вторник");
+                    break;
+                case DayOfWeek.WED:
+                    Console.WriteLine("Среда");
+                    break;
+                case DayOfWeek.THU:
+                    Console.WriteLine("Четверг");
+                    break;
+                case DayOfWeek.FRI:
+                    Console.WriteLine("Пятница");
+                    break;
+                case DayOfWeek.SAT:
+                    Console.WriteLine("Суббота");
+                    break;
+                case DayOfWeek.SUN:
+                    Console.WriteLine("Всокресенье");
+                    break;
+            }
+        }
+        static DayOfWeek ReadDayOfWeek()
+        {
+            int day;
+            day = int.Parse(Console.ReadLine());
+            DayOfWeek dow = DayOfWeek.MON;
+            switch (day)
+            {
+                case 1:
+                    dow = DayOfWeek.MON;
+                    break;
+                case 2:
+                    dow = DayOfWeek.TUE;
+                    break;
+                case 3:
+                    dow = DayOfWeek.WED;
+                    break;
+                case 4:
+                    dow = DayOfWeek.THU;
+                    break;
+                case 5:
+                    dow = DayOfWeek.FRI;
+                    break;
+                case 6:
+                    dow = DayOfWeek.SAT;
+                    break;
+                case 7:
+                    dow = DayOfWeek.SUN;
+                    break;
+            }
+            return dow;
+        }
+        static void Main()
+        {
+            DayOfWeek dow = ReadDayOfWeek();
+            PrintDayOfWeek(dow);
+        }
+       
+    }
+}
---
+++ 6 урок форма.cs
@@ -0,0 +1,97 @@
+﻿namespace urok__6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pct
+            // 
+            this.pct.Image = ((System.Drawing.Image)(resources.GetObject("pct.Image")));
+            this.pct.Location = new System.Drawing.Point(71, 60);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(312, 161);
+            this.pct.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pct.TabIndex = 0;
+            this.pct.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "btnStart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "btnExit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pct);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
